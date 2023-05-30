89af9dffeaad05be53048aca570ddf24efcae736
meghaph <finserve.pvtltd@gmail.com>
2023-05-30 17:27:35 +0530

    forgot-pwd

--- src/app/register-login/register-login-routing.module.ts
+++ src/app/register-login/register-login-routing.module.ts
@@ -4,12 +4,14 @@ import { RegisterLoginComponent } from './register-login.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from '../home-page/home/home.component';
+import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
 
 const routes: Routes = [
   { path: '', component: RegisterLoginComponent },
   { path: 'login', component: LoginComponent },
   {path:'register',component:RegisterComponent},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'forgotpassword', component:ForgotPwdComponent}
 ];
 
 @NgModule({
